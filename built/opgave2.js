0c19b2d694dce8d1f3c0dbeb3c1bac74114cc0c1
Kamilla <113370998+Kanin60@users.noreply.github.com>
2024-01-09 09:00:47 +0100

    Opgave2

---
+++ built/opgave2.js
@@ -0,0 +1,19 @@
+"use strict";
+/*
+Skal indeholde følgende typer:
+string
+boolean
+number
+undefined
+null
+*/
+//STRING
+let myString = "tekst";
+//BOOLEAN
+let myBoolean = true;
+// NUMBER
+let myNumber = 10;
+//UNDEFINED
+let myUndefined = undefined;
+//NULL
+let myNull = null;
---
+++ src/opgave2.ts
@@ -0,0 +1,24 @@
+/* 
+Skal indeholde følgende typer:
+string
+boolean
+number
+undefined
+null 
+*/
+
+//STRING
+let myString: string = "tekst";
+
+//BOOLEAN
+let myBoolean: boolean = true;
+
+// NUMBER
+let myNumber: number = 10;
+
+//UNDEFINED
+let myUndefined: undefined = undefined;
+
+//NULL
+let myNull: null = null;
+
